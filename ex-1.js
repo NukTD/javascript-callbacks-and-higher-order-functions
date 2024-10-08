bd9e31e15a0dc6a6a0377a5e31c2a6ad4046f052
Thammaruthai <thammaruthai@gmail.com>
2024-10-08 14:16:30 +0700

    feat: Answer ex1 and ex2

--- ex-1.js
+++ ex-1.js
@@ -7,6 +7,40 @@ function forEach(array, operation) {
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
+//-----------------------------------------------------------------------------------------------------------------------
+
+//function increase salary by 5,000
+// Agrement is Salary as Number
+function salaryUp5000(number){
+  return number+5000;
+}
+
+//function HOF for promote Salary
+// 1st Agrement is Salary Promote. (how many salary will increase
+// 2nd Agrement is base salary list as Array 
+// 3rd Agrement is New salary list that we would like to reassign in Array
+function promoteEmployees(callback,employeeSalaryList,newSalaryList){
+  for(let i=0;i<employeeSalaryList.length;i++){
+    
+    
+    //console.log(i);   //check i
+    //console.log(employeeSalaryList[i])   //check employeeSalaryList
+    //console.log(salaryUp5000(employeeSalaryList[i]));   // check new salary each index
+    
+    newSalaryList[i]=(salaryUp5000(employeeSalaryList[i]));
+  }
+
+  //console.log(newSalaryList);   //check result
+  return newSalaryList;
+}
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+
 // Using `forEach` function here
 
+promoteEmployees(salaryUp5000,employeeSalaries,newEmployeeSalaries); // execute the function
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
+
+
+
--- ex-2.js
+++ ex-2.js
@@ -2,14 +2,43 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
-}
+  let totalPassStudent=0;
+  let classRoom ="";
+  for(let i=0;i<array.length;i++){
+    operation(70,array[i])=== true ? totalPassStudent++: totalPassStudent=totalPassStudent
+  }
+  
+  //console.log(totalPassStudent);
+  //console.log(totalPassStudent>=5 ? "ผ่านเกณฑ์ ✅" : "ไม่ผ่านเกณฑ์ ❌")
+  totalPassStudent>=5 ? classRoom ="ผ่านเกณฑ์ ✅" : classRoom ="ไม่ผ่านเกณฑ์ ❌"
+  return classRoom;
 
+}
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
+
+
+// ฟังก์ชั่นเช็ค เกณฑ์ผ่านรายบุคคล ใส่ เกณฑ์คะแนน และ คะแนนนักเรียน
+function individualPassQualify(qualifyScore,studentScore){
+  return studentScore>=qualifyScore ? true: false;
+}
+
+
 
 // Using `atLeastFive` function here
 
 let scoreRoom1Result;
 let scoreRoom2Result;
 let scoreRoom3Result;
+
+scoreRoom1Result=atLeastFive(studentScoresRoom1,individualPassQualify)
+console.log("นักเรียนห้องที่ 1 "+scoreRoom1Result)
+
+scoreRoom1Result=atLeastFive(studentScoresRoom2,individualPassQualify)
+console.log("นักเรียนห้องที่ 2 "+scoreRoom1Result)
+
+scoreRoom1Result=atLeastFive(studentScoresRoom3,individualPassQualify)
+console.log("นักเรียนห้องที่ 3 "+scoreRoom1Result)
+
+
